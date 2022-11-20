8ff5853e62fe4f93cea2fa8344a73a44e84cb8ed
ap-dyas-i-masruri <dyas.i.masruri@astrapay.com>
2022-11-20 09:50:26 +0700

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/t/p/**'
+      }
+    ]
+  }
 }
